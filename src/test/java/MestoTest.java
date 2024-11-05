1a17103ff18c3eddf5f6ff4e9fbf3139d8df3975
Evgeniy Chernyshev <chernyshevqa@gmail.com>
2024-11-05 10:04:56 +0300

    add personal token

--- src/test/java/MestoTest.java
+++ src/test/java/MestoTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 
 public class MestoTest {
-    String bearerToken = "сюда_впиши_свой_токен";
+    String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmQ5Y2Q4NDBjODlhYTAwM2QzYzA2YzIiLCJpYXQiOjE3MzA3OTAxMDEsImV4cCI6MTczMTM5NDkwMX0.kbPsMPU2AICMyMdJRgjhIxwwTbh4P8D98sFZXIbkAak";
 
     @Before
     public void setUp() {
